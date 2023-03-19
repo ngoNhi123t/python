7dcbd3389c00bcd26fb14f561aeffb67805cb409
ngoNhi123t <110970984+ngoNhi123t@users.noreply.github.com>
2023-03-19 16:03:41 +0700

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,12 @@
+#1
+# name = input('Enter your name:')
+# print('Hello ' + name)
+# #2
+# hours = input('Enter hours: ')
+# rates = input('Enter Rate: ')
+# pay = float(hours) * float(rates)
+# print('Pay:' ,pay)
+#5
+c= input('nhập nhiệt độ(C):')
+f = float(c) + 273.0
+print(f,"F")
