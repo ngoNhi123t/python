5c5d5f89212d72d9e8d2083d45c2f7f11a2fd10e
ngoNhi123t <110970984+ngoNhi123t@users.noreply.github.com>
2023-03-19 16:04:59 +0700

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,12 @@
+# str = 'X-DSPAM-Confidence:0.8475'
+# index = str.find(':')
+# print(float(str[index+1:]))
+# print(len(str))
+# count = 0
+# for i in str:
+#     count = count +1
+# print(count)
+
+'a' in 'X-DSPAM-Confidence:0.8475'
+#Ví dụ sau sử dụng "%d" để định dạng một số nguyên, "%g" để định dạng một số dấu phẩy động (đừng hỏi tại sao) và "%s" để định dạng một chuỗi:
+'In %d years I have spotted %g %s.' % (3, 0.1, 'camels')
